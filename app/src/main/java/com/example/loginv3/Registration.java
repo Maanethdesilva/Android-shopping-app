737035d2c6a77b0f313692c801fe12933a41027e
ArielleRamgoolie <arielleramgoolie27@gmail.com>
2021-11-29 17:41:15 -0500

    Arielle - updated UI for login and register page - created  a new Java class called User which allows the user information to be read, validated and authenticated by firebase

---
+++ app/src/main/java/com/example/loginv3/Registration.java
@@ -0,0 +1,36 @@
+package com.example.loginv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Registration extends AppCompatActivity implements View.OnClickListener {
+
+//    private EditText name, email, password;
+    private TextView register;
+
+    //initialize firebase authentication
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_v1);
+
+        register = (Button) findViewById(R.id.registerbutton);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerbutton:
+                startActivity(new Intent(this, User.class));
+        }
+    }
+}
