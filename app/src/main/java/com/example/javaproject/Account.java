ea5164fb8b682d49a7b997756b612cea8179b8e7
your_username <arielleramgoolie27@gmail.com>
2021-12-02 01:19:30 -0500

    Arielle - Linked Registration to firebase so that users with emails and passwords are created.

---
+++ app/src/main/java/com/example/javaproject/Account.java
@@ -0,0 +1,11 @@
+package com.example.javaproject;
+
+public class Account {
+    public String fullname;
+    public String email;
+
+    public Account(String fullname, String email){
+        this.fullname = fullname;
+        this.email = email;
+    }
+}
