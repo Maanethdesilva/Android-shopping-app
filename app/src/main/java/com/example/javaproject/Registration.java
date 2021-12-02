b7e9129516dc6d9c8a62bb924dc5356a66c749c7
your_username <arielleramgoolie27@gmail.com>
2021-12-02 14:58:43 -0500

    Arielle - updated registration for both customer and StoreOwner to accept phone number and redirected successful registrations to login page

--- app/src/main/java/com/example/javaproject/Registration.java
+++ app/src/main/java/com/example/javaproject/Registration.java
@@ -24,7 +24,7 @@ import java.util.regex.Pattern;
 public class Registration extends AppCompatActivity implements View.OnClickListener {
     private Button register;
     private FirebaseAuth mAuth;
-    private EditText eTName, eTEmail, eTPassword;
+    private EditText eTFirstName, eTLastName, eTPhoneNumber, eTEmail, eTPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,41 +34,39 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         register = (Button) findViewById(R.id.registerbutton);
         register.setOnClickListener(this);
 
-        eTName = (EditText) findViewById(R.id.editTextName);
+        eTFirstName = (EditText) findViewById(R.id.editTextName);
+        eTLastName = (EditText) findViewById(R.id.editTextName2);
+        eTPhoneNumber = (EditText) findViewById(R.id.editTextTextPhoneNumber);
         eTEmail = (EditText) findViewById(R.id.emailAddress);
         eTPassword = (EditText) findViewById(R.id.editTextPasswordReg);
-
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.registerbutton){
-            register(eTName, eTEmail, eTPassword);
+            register(eTFirstName, eTLastName, eTPhoneNumber, eTEmail, eTPassword);
+            startActivity(new Intent(this, MainActivity.class));
         }
     }
 
-    public void register(EditText eTname, EditText eTemail, EditText eTpassword){
+    public void register(EditText eTFirstName, EditText eTLastName, EditText eTPhoneNumber, EditText eTemail, EditText eTpassword){
+        String firstName = eTFirstName.getText().toString().trim();
+        String lastName = eTLastName.getText().toString().trim();
+        String phoneNumber = eTPhoneNumber.getText().toString().trim();
         String email = eTemail.getText().toString();
         String password = eTpassword.getText().toString();
-        String name = eTname.getText().toString();
 
-//        if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
-//            Toast.makeText(Registration.this, "Please enter all information", Toast.LENGTH_LONG).show();
-//            eTname.requestFocus();
-//            return;
-//        }
-//        Pattern pattern = Pattern.compile(".com");
-//        Matcher matcher = pattern.matcher(email);
-//        if (!matcher.matches()){
-//            eTemail.setError("Email Address invalid");
-//            eTemail.requestFocus();
-//            return;
-//        }
-        if (name.isEmpty()){
-            eTname.setError("Field is empty");
-            eTname.requestFocus();
+        if (firstName.isEmpty()){
+            eTFirstName.setError("Field is empty");
+            eTFirstName.requestFocus();
             return;
         }
+        if (lastName.isEmpty()){
+            eTLastName.setError("Field is empty");
+            eTLastName.requestFocus();
+            return;
+        }
+
         if (email.isEmpty()){
             eTemail.setError("Field is empty");
             eTemail.requestFocus();
@@ -84,36 +82,14 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-        //the following line of code was taken from youtube vid
-//        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//                if (task.isSuccessful()){
-//                    Account user = new Account (name, email);
-//                    String user_ref = FirebaseAuth.getInstance().getCurrentUser().getUid();
-//                    FirebaseDatabase.getInstance().getReference("Users").child(user_ref).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<Void> task) {
-//                            if (task.isSuccessful()){
-//                                Toast.makeText(Registration.this, "successful registration", Toast.LENGTH_SHORT).show();
-//                            }
-//                            else{
-//                                Toast.makeText(Registration.this, "Registration failed", Toast.LENGTH_SHORT).show();
-//                            }
-//                        }
-//                    });
-//                }else{
-//                    Toast.makeText(Registration.this, "Registration failed", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
         mAuth.createUserWithEmailAndPassword(email, password);
         String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         HashMap<String, Object> user = new HashMap<String, Object>();
-        user.put("Name", name);
-        user.put("Email", email);
-        DatabaseReference user_ref = FirebaseDatabase.getInstance().getReference().child("Users2");
+        user.put("First Name", firstName);
+        user.put("Last Name", lastName);
+        user.put("Phone Number", phoneNumber);
+        user.put("Email Address", email);
+        DatabaseReference user_ref = FirebaseDatabase.getInstance().getReference().child("Users");
         user_ref.child(userID).updateChildren(user);
-        //Create a customer account
     }
 }
