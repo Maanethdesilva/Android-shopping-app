53da07d4c73dd0212c2f372ee0e71419f4b4a00d
your_username <arielleramgoolie27@gmail.com>
2021-12-02 14:58:43 -0500

    Arielle - updated registration for both customer and StoreOwner to accept phone number and redirected successful registrations to login page

--- app/src/main/java/com/example/javaproject/Account.java
+++ app/src/main/java/com/example/javaproject/Account.java
@@ -1,11 +1,13 @@
 package com.example.javaproject;
 
 public class Account {
-    public String fullname;
+    public String fName;
+    public String lName;
     public String email;
 
-    public Account(String fullname, String email){
-        this.fullname = fullname;
+    public Account(String fName, String lName, String email){
+        this.fName = fName;
+        this.lName = lName;
         this.email = email;
     }
 }
--- app/src/main/java/com/example/javaproject/MainActivity.java
+++ app/src/main/java/com/example/javaproject/MainActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button signup, loginbutton;
+    private EditText eTEmail, eTPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signup.setOnClickListener(this);
         loginbutton = (Button) findViewById(R.id.loginbutton);
         loginbutton.setOnClickListener(this);
+
+        eTEmail = (EditText) findViewById(R.id.signInEmailAddress);
+        eTPassword = (EditText) findViewById(R.id.signInPassword);
     }
 
     //arielle changed switch case to if statements
@@ -29,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(this, signupOptions.class));
         }
         if (v.getId() == R.id.loginbutton){
-            startActivity(new Intent(this, StoreOwnerActivity.class));
+
         }
     }
 }
