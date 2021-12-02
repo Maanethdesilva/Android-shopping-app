8b2a44177fd8991ff043d36c1e2023801822d093
magenhc <magenchen1@gmail.com>
2021-12-02 11:39:51 -0500

    magen - linked list of all stores on the customer home page to the database.

--- app/src/main/java/com/example/javaproject/CustomerActivity.java
+++ app/src/main/java/com/example/javaproject/CustomerActivity.java
@@ -1,17 +1,66 @@
 package com.example.javaproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.cert.PolicyNode;
+import java.util.ArrayList;
+
 
 public class CustomerActivity extends AppCompatActivity {
+    private static final String TAG = "CustomerActivity";
+
+
+    //UI references
+
+    //private Button orderButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+
+        ListView storeListView = (ListView) findViewById(R.id.custListView);
+        ArrayList<Store> allStores = new ArrayList<Store>();
+
+        CustomerListAdapter adapter = new CustomerListAdapter(this, R.layout.adapter_view_customer, allStores);
+        storeListView.setAdapter(adapter);
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("Stores");
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                allStores.clear();
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    Store store = new Store(snapshot.child("Storename").getValue().toString());
+                    allStores.add(store);
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Log.d("LIST IS ", allStores.toString());
+
+
     }
     public void viewProfile(View v){
         startActivity(new Intent(CustomerActivity.this, ProfilePage.class));
@@ -22,4 +71,5 @@ public class CustomerActivity extends AppCompatActivity {
     public void viewOrder(View v){
         startActivity(new Intent(CustomerActivity.this, OrderPage.class));
     }
+
 }
--- app/src/main/java/com/example/javaproject/ProfilePage.java
+++ app/src/main/java/com/example/javaproject/ProfilePage.java
@@ -1,14 +1,38 @@
 package com.example.javaproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ProfilePage extends AppCompatActivity {
+    private static final String TAG = "ProfilePage";
+    private Button btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.notification_page);
+        setContentView(R.layout.profile_page);
+        btnSignOut = (Button) findViewById(R.id.custSignOut);
+
+
+
+
+
+
     }
+    public void viewCustomerHome(View v){
+        startActivity(new Intent(ProfilePage.this, CustomerActivity.class));
+    }
+
+
+
 }
