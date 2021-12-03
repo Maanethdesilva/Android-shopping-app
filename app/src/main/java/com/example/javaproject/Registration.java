38b6764aae7898ed47e90bc9572d578be7b173fe
your_username <arielleramgoolie27@gmail.com>
2021-12-02 20:29:02 -0500

    Arielle - updated login

--- app/src/main/java/com/example/javaproject/Registration.java
+++ app/src/main/java/com/example/javaproject/Registration.java
@@ -1,28 +1,20 @@
 package com.example.javaproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Registration extends AppCompatActivity implements View.OnClickListener {
-    private Button register;
     private FirebaseAuth mAuth;
     private EditText eTFirstName, eTLastName, eTPhoneNumber, eTEmail, eTPassword;
 
@@ -31,21 +23,20 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_v1);
         mAuth = FirebaseAuth.getInstance();
-        register = (Button) findViewById(R.id.registerbutton);
+        Button register = findViewById(R.id.registerbutton);
         register.setOnClickListener(this);
 
-        eTFirstName = (EditText) findViewById(R.id.editTextName);
-        eTLastName = (EditText) findViewById(R.id.editTextName2);
-        eTPhoneNumber = (EditText) findViewById(R.id.editTextTextPhoneNumber);
-        eTEmail = (EditText) findViewById(R.id.emailAddress);
-        eTPassword = (EditText) findViewById(R.id.editTextPasswordReg);
+        eTFirstName = findViewById(R.id.editTextName);
+        eTLastName = findViewById(R.id.editTextName2);
+        eTPhoneNumber = findViewById(R.id.editTextTextPhoneNumber);
+        eTEmail = findViewById(R.id.emailAddress);
+        eTPassword = findViewById(R.id.editTextPasswordReg);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.registerbutton){
             register(eTFirstName, eTLastName, eTPhoneNumber, eTEmail, eTPassword);
-            startActivity(new Intent(this, MainActivity.class));
         }
     }
 
@@ -82,14 +73,26 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-        mAuth.createUserWithEmailAndPassword(email, password);
-        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        HashMap<String, Object> user = new HashMap<String, Object>();
-        user.put("First Name", firstName);
-        user.put("Last Name", lastName);
-        user.put("Phone Number", phoneNumber);
-        user.put("Email Address", email);
-        DatabaseReference user_ref = FirebaseDatabase.getInstance().getReference().child("Users");
-        user_ref.child(userID).updateChildren(user);
+        mAuth.fetchSignInMethodsForEmail(email)
+                .addOnCompleteListener(task -> {
+                    boolean isNotExistingEmail = task.getResult().getSignInMethods().isEmpty();
+                    if (isNotExistingEmail) {
+                        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task1 -> {
+                            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            HashMap<String, Object> user = new HashMap<>();
+                            user.put("First Name", firstName);
+                            user.put("Last Name", lastName);
+                            user.put("Phone Number", phoneNumber);
+                            user.put("Email Address", email);
+                            user.put("isStoreOwner", false);
+                            DatabaseReference user_ref = FirebaseDatabase.getInstance().getReference().child("Users");
+                            user_ref.child(userID).updateChildren(user);
+                            startActivity(new Intent(this, MainActivity.class));
+                        });
+                    } else {
+                        eTemail.setError("This email is linked to an existing account");
+                        eTemail.requestFocus();
+                    }
+                });
     }
 }
